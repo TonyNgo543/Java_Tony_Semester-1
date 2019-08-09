2cb65735976b2c878c9ee384b0ffada1da40c61b
TonyNgo543 <53871987+TonyNgo543@users.noreply.github.com>
2019-08-09 08:19:46 -0700

    Update Hello_Tony.java

--- Hello_Tony.java
+++ Hello_Tony.java
@@ -1,3 +1,9 @@
+/*This is my first Java program
+    */
+
+
+
+
 public class Hello_Tony {
     public static void main(String[] args) {
         System.out.println("Hello Future Communist Party Leader");
